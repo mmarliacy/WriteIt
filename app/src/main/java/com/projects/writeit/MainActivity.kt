327e4f4a572ea94195060d81bf9948e52cbbc00b
Mel <melanie.marliacy@gmail.com>
2024-12-05 06:08:38 +0100

    WDID : _ Place button on screen _ Connect "Add" Button with Modal Bottom Sheet
    WTD :
    _ Add an element to list
    _ Delete element from list

--- app/src/main/java/com/projects/writeit/MainActivity.kt
+++ app/src/main/java/com/projects/writeit/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.projects.writeit
 
 import android.os.Bundle
-import android.service.autofill.OnClickAction
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
@@ -22,26 +18,37 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomSheetScaffoldState
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.projects.writeit.ui.theme.WriteItTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     private var shopList = ShopList.list
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,6 +63,15 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WriteItTheme {
+        ProductItem("ok", modifier = Modifier)
+    }
+}
+
 
 
 @Composable
@@ -74,41 +90,62 @@ fun ProductItem(product: String, modifier: Modifier) {
 
 }
 
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShopList(list: List<String>, modifier: Modifier) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-            .windowInsetsPadding(WindowInsets.statusBars).fillMaxSize()
-    ) {
-        LazyColumn(verticalArrangement = Arrangement.spacedBy(2.dp)) {
-            items(list) { product ->
-                ProductItem(product, modifier)
-            }
-        }
-        Box(contentAlignment = Alignment.BottomEnd) {
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember {
+    mutableStateOf(false)
+    }
+    Scaffold(
+        floatingActionButton = {
             FloatingActionButton(
                 containerColor = Color.Black,
                 contentColor = Color.White,
-                onClick = { AddProduct() }
+                onClick = {
+                    showBottomSheet = true
+                }
             ) {
                 Icon(Icons.Filled.Add, "Add floating action button")
             }
         }
 
-    }
-}
-
-
-fun AddProduct() {
-    TODO("Not yet implemented")
-}
-
-
-@Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        WriteItTheme {
-            ProductItem("ok", modifier = Modifier)
+    ) { innerPadding ->
+        if (showBottomSheet){
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                sheetState = sheetState
+            ) {
+                Button(onClick = {
+                    scope.launch { sheetState.hide() }.invokeOnCompletion {
+                        if (!sheetState.isVisible) {
+                            showBottomSheet = false
+                        }
+                    }
+                }) {
+                    Text("Hide bottom sheet")
+                }
+            }
         }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .windowInsetsPadding(WindowInsets.statusBars).fillMaxSize().padding(innerPadding)
+        ) {
+            LazyColumn(verticalArrangement = Arrangement.spacedBy(2.dp)) {
+                items(list) { product ->
+                    ProductItem(product, modifier)
+                }
+            }
+        }
+
     }
+
+}
+
+
